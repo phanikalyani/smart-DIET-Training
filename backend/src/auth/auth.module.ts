79cfecc3f0c6ec48e986f144de1413bc9fdda844
phanikalyani <39437327+phanikalyani@users.noreply.github.com>
2026-01-11 08:05:16 +0530

    commit

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      secret: 'SECRET_KEY_DIET',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  providers: [AuthService],
+  controllers: [AuthController],
+  exports: [AuthService],
+})
+export class AuthModule {}
